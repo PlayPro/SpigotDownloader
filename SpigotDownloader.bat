7302f27bdb4dce35a57c493cbafcc5e922ee812d
Intelli <6790859+Intelli@users.noreply.github.com>
2022-12-22 15:20:32 -0700

    Update to 1.19.3

--- SpigotDownloader.bat
+++ SpigotDownloader.bat
@@ -4,8 +4,8 @@ set buildtools=%cd%\BuildTools
 if not exist "%buildtools%" mkdir %buildtools%
 bitsadmin.exe /transfer "BuildTools" https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar %buildtools%\BuildTools.jar
 cd %buildtools%
-java -Xmx1024M -jar BuildTools.jar --rev 1.17
+java -Xmx1024M -jar BuildTools.jar --rev 1.19.3 --compile SPIGOT
 cd ..
-xcopy /y %buildtools%\spigot-1.17.jar %cd%
+xcopy /y %buildtools%\spigot-1.19.3.jar %cd%
 
 PAUSE
